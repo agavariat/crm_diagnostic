cf456d41968cc33732cb620731c2015d06f8f2c1
daniel giraldo <agaviriat@gmail.com>
2021-04-01 17:07:10 -0500

    h

--- models/__init__.py
+++ models/__init__.py
@@ -5,4 +5,4 @@ from . import crm_diagnostic
 from . import crm_diagnostic_line
 from . import inherit_base_user_role
 from . import inherit_crm_stage
-from . import crm_attention_plan
+#from . import crm_attention_plan
