3a0b6ddf81efaff6ff31cf8c6bc6326d195cb19b
daniel giraldo <agaviriat@gmail.com>
2021-04-01 22:45:40 -0500

    j

--- __manifest__.py
+++ __manifest__.py
@@ -23,11 +23,9 @@ CRM Diagnostic
         'views/crm_lead_view.xml',
         'views/crm_diagnostic_view.xml',
         'views/inherit_base_user_role_view.xml',
-        'views/crm_attention_plan_view.xml',
         'report/crm_diagnostic_report_template.xml',
         'report/crm_diagnostic_report.xml',
-        'report/crm_attention_plan_template.xml',
-        'report/crm_attention_plan_report.xml',
+        
     ],
     'installable': True,
     'auto_install': False,
